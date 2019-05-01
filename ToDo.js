cb22cdafd710f84a15fdd1bf874680bb7eac581b
trapa <1982gaya@gmail.com>
2019-05-02 00:44:10 +0900

    UI #2 - input & To Do Components

---
+++ ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text, touchableOpacity, StyleSheet } from 'react-native';
+
+export default class ToDo extends Component {
+    render () {
+        return (
+            <View>
+                <Text>To Do</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create ({})
