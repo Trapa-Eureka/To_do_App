fe75bfa94d6d105189b11a6ae64763ab5aa26162
trapa <1982gaya@gmail.com>
2019-05-02 10:47:53 +0900

    UI #3 - Styling To Do Component

--- ToDo.js
+++ ToDo.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text, touchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, touchableOpacity, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+
+const { width, hight } = Dimensions.get("window");
 
 export default class ToDo extends Component {
+    state = {
+        isEditing: false,
+        isCompleted: false
+    };
     render () {
+        const { isCompleted } = this.state;
         return (
-            <View>
-                <Text>To Do</Text>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={this._toggleComplete}>
+                    <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
+                </TouchableOpacity>
+                <Text style={styles.text}>To Do</Text>
             </View>
-        )
+        );
     }
+    _toggleComplete = () => {
+        this.setState(prevState => {
+            return {
+                isCompleted: !prevState.isCompleted
+            };
+        });
+    };
 }
 
-const styles = StyleSheet.create ({})
+const styles = StyleSheet.create ({
+    container: {
+        width: width - 50,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    circle: {
+        width: 26,
+        height: 26,
+        borderRadius: 13,
+        borderWidth: 3,
+        marginRight: 20
+    },
+    completedCircle: {
+        borderColor: "#bbb"
+    },
+    uncompletedCircle: {
+        borderColor: "#f23657"
+    },
+    text: {
+        fontWeight: "600",
+        fontSize: 20,
+        marginVertical: 20
+    }
+})
